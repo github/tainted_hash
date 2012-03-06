eb85ebb8ea1f860e09336e1d8c3f85a65855db4c
risk :danger: olson <technoweenie@gmail.com>
2012-03-05 19:20:52 -0800

    nfi if this rails hack works (I'm on a plane)

---
+++ lib/rails_init.rb
@@ -0,0 +1,1 @@
+require File.expand_path("../tainted_hash/rails", __FILE__)
---
+++ lib/tainted_hash/rails.rb
@@ -0,0 +1,15 @@
+require File.expand_path("../../tainted_hash", __FILE__)
+
+if Object.const_defined?(:Rails)
+  case Rails.version
+  when /^2\.3\./
+    class ActionController::Request
+      alias rails_parameters parameters
+      def parameters
+        @parameters ||= TaintedHash.new(rails_parameters)
+      end
+    end
+  end
+else
+  raise "Not a supported Rails project"
+end
