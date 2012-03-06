32d116a19097b6de8366f1007bfcec953e42edec
risk :danger: olson <technoweenie@gmail.com>
2012-03-06 14:41:15 -0800

    teak how we wrap Rails' #params

--- lib/tainted_hash/rails.rb
+++ lib/tainted_hash/rails.rb
@@ -1,15 +1,12 @@
 require File.expand_path("../../tainted_hash", __FILE__)
 
-if Object.const_defined?(:Rails)
-  case Rails.version
-  when /^2\.3\./
-    class ActionController::Request
-      alias rails_parameters parameters
-      def parameters
-        @parameters ||= TaintedHash.new(rails_parameters, nil, nil, HashWithIndifferentAccess)
-      end
-    end
+module TaintedHash::Controller
+  def wrap_params_with_tainted_hash
+    @_params = TaintedHash.new(@_params.to_hash)
   end
-else
-  raise "Not a supported Rails project"
 end
+
+if defined?(ActionController::Base)
+  ActionController::Base.send :include, TaintedHash::Controller
+end
+
